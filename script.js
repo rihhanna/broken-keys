3fcc1680835e105d2a160bb57a7b60b44b69f21d
rihhana <hrihhana@gmail.com>
2023-08-25 19:18:06 +0300

    done it

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Good Luck 💪🏾
+function brokenKeys(correct, typed) {
+    // create an empty set to store the broken keys
+    let broken = new Set();
+    // loop through the correct phrase
+    for (let i = 0; i < correct.length; i++) {
+      // check if the corresponding character in the typed phrase is different
+      if (correct[i] !== typed[i]) {
+        // add the correct character to the broken set
+        broken.add(correct[i]);
+      }
+    }
+    // return the broken set as an array
+    return [...broken];
+  }
+  console.log(brokenKeys("fikrcamp bootcamp", "fikrcomp boorcamp"))
+  console.log(brokenKeys("good night", "gppd night"))
